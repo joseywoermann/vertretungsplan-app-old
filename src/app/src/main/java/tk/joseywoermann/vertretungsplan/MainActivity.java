7c7c7b16596b82a9bf9827d5762e655c36b87b7b
joseywoermnn <jcw05@gmx.de>
2020-12-10 20:31:05 +0100

    removed top bar

--- src/app/src/main/java/tk/joseywoermann/vertretungsplan/MainActivity.java
+++ src/app/src/main/java/tk/joseywoermann/vertretungsplan/MainActivity.java
@@ -2,25 +2,26 @@ package tk.joseywoermann.vertretungsplan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String plan1 = "http://www.kreisgymnasium-halle.de/wp-content/uploads/Service/vertretungsplan/subst_001.htm";
-        String plan2 = "http://www.kreisgymnasium-halle.de/wp-content/uploads/Service/vertretungsplan/subst_002.htm";
-        String plan3 = "http://www.kreisgymnasium-halle.de/wp-content/uploads/Service/vertretungsplan/subst_003.htm";
-        String plan4 = "http://www.kreisgymnasium-halle.de/wp-content/uploads/Service/vertretungsplan/subst_004.htm";
-        String plan5 = "http://www.kreisgymnasium-halle.de/wp-content/uploads/Service/vertretungsplan/subst_005.htm";
-        WebView vertretungsplan_wv = findViewById(R.id.vertretungsplan_wv);
+        String page1 = "http://www.kreisgymnasium-halle.de/wp-content/uploads/Service/vertretungsplan/subst_001.htm";
+        String page2 = "http://www.kreisgymnasium-halle.de/wp-content/uploads/Service/vertretungsplan/subst_002.htm";
+        String page3 = "http://www.kreisgymnasium-halle.de/wp-content/uploads/Service/vertretungsplan/subst_003.htm";
+        String page4 = "http://www.kreisgymnasium-halle.de/wp-content/uploads/Service/vertretungsplan/subst_004.htm";
+        String page5 = "http://www.kreisgymnasium-halle.de/wp-content/uploads/Service/vertretungsplan/subst_005.htm";
+
+        String currentPage = "page1";
+
+        WebView vertretungsplan_wv = (WebView)findViewById(R.id.vertretungsplan_wv);
         vertretungsplan_wv.getSettings().setJavaScriptEnabled(true);
-        vertretungsplan_wv.loadUrl(plan1);
+        vertretungsplan_wv.loadUrl(page1);
     }
 }
